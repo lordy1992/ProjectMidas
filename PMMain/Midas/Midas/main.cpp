af5eca2d9fa6eaf71aa59b221ec6e644de1934b1
Jorden Hetherington <jorden.hetherington@gmail.com>
2014-11-02 14:17:10 -0800

    cleaning up a tad

--- PMMain/Midas/Midas/main.cpp
+++ PMMain/Midas/Midas/main.cpp
@@ -28,59 +28,6 @@ int main(void) {
 		Sleep(1000);
 	}
 
-
-	// MANUAL TEST 2
-	cout << "Running Midas Input Control, please use 'w'/'a'/'s'/'d' to control mouse movement, or \"exit\" to leave." << endl;
-
-	int scale = 20;
-	string input;
-	MOUSEINPUT mi;
-	INPUT mi_arr[2];
-	while (true) {
-		cin >> input;
-		if (input == "exit") {
-			break;
-		}
-
-		ZeroMemory(&mi, sizeof(MOUSEINPUT));
-		mi.dwFlags = MOUSEEVENTF_MOVE;
-		if (input == "a") {
-			mi.dx = -1;
-			mi.dy = 0;
-		}
-		else if (input == "s") {
-			mi.dx = 0;
-			mi.dy = 1;
-		}
-		else if (input == "d") {
-			mi.dx = 1;
-			mi.dy = 0;
-		}
-		else if (input == "w") {
-			mi.dx = 0;
-			mi.dy = -1;
-		}
-		else if (input == "cl") {
-			mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
-			mi_arr[0].type = INPUT_MOUSE;
-			mi_arr[0].mi = mi;
-			mi.dwFlags = MOUSEEVENTF_LEFTUP;
-			mi_arr[1].type = INPUT_MOUSE;
-			mi_arr[1].mi = mi;
-		}
-
-		if (input != "cl") {
-			mi.dx *= scale;
-			mi.dy *= scale;
-			mi_arr[0].type = INPUT_MOUSE;
-			mi_arr[0].mi = mi;
-			SendInput(1, mi_arr, sizeof(INPUT));
-		}
-		else {
-			SendInput(2, mi_arr, sizeof(INPUT));
-		}
-	}
-
 	system("PAUSE");
 	return 0;
 }
