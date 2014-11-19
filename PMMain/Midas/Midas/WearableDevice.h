5f388e19572d62b46750c26b24efd4a9fc3917c8
lordy1992 <jlord_2006@hotmail.com>
2014-11-18 23:28:33 -0800

    Documenting the last few header files

--- PMMain/Midas/Midas/WearableDevice.h
+++ PMMain/Midas/Midas/WearableDevice.h
@@ -5,36 +5,72 @@
 
 class WearableDevice;
 
+/**
+ * Kicks off the wearable device thread.
+ *
+ * @param wearableDevice The device to start running.
+ */
 void startWearableDeviceListener(WearableDevice* wearableDevice);
 
+/**
+ * The status of the device, that can be returned after the device has finished
+ * running.
+ */
 enum class deviceStatus
 {
-    RUNNING,
-    DONE,
-    ERR
+    RUNNING,    /**< The device is still running. */
+    DONE,       /**< The device has finished without error. */
+    ERR         /**< The device has finished with an error. */
 };
 
+/**
+ * Handles a specific Midas device. 
+ */
 class WearableDevice 
 {
 public:
     WearableDevice(SharedCommandData* sharedCommandData) : sharedData(sharedCommandData), stopFlag(false) { }
+
+    /**
+     * Runs the device loop, handling specific event handling for the device.
+     */
     virtual void runDeviceLoop() = 0;
 
-    // Not necessarily thread-safe. This should only be called once
-    // the device thread has stopped. The error code is device-specific.
+    /**
+     * This returns a device-specific error code from the device, once it has
+     * stopped running. This is not necessarily thread-safe and should only be 
+     * called once the device thread has stopped.
+     *
+     * @return The device-specific error code.
+     */
     virtual int getDeviceError() = 0;
 
-    // Thread-safe
+    /**
+     * Returns the status of the device. This is thread-safe.
+     *
+     * @return The status of the device.
+     */
     deviceStatus getDeviceStatus();
+
+    /**
+     * Stops the device. This gives the device a chance to respond 
+     * and perform cleanup before exiting.
+     */
     void stopDevice();
 
 protected:
+    /**
+     * Sets the status of the device.
+     *
+     * @param newStatus The new status of the device.
+     */
     void setDeviceStatus(deviceStatus newStatus);
 
-    // It is the device's responsibility to check this method to 
-    // determine if the main thread has requested it to stop. If it has
-    // been sent a stop request, the device must perform cleanup and exit its
-    // main loop.
+    /**
+     * Indicates if the device has been requested to stop (by the main thread). It is
+     * the device's responsibility to check this method and, if requested to stop, perform
+     * the necessary cleanup and exit the main loop.
+     */
     bool stopDeviceRequested();
 
     SharedCommandData* sharedData;
