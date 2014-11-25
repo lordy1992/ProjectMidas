6992844241383ed3acc593e7a4dd9f35e483e01d
Jorden Hetherington <jorden.hetherington@gmail.com>
2014-11-24 23:01:14 -0800

    continuing gestureSeqRecorder... small commits

--- PMMain/Midas/Midas/GestureSeqRecorder.cpp
+++ PMMain/Midas/Midas/GestureSeqRecorder.cpp
@@ -49,6 +49,7 @@ SequenceStatus GestureSeqRecorder::registerSequence(midasMode mode, sequence seq
 SequenceStatus GestureSeqRecorder::progressSequence(myo::Pose::Type gesture, ControlState state, sequenceResponse& response)
 {
     SequenceStatus status = SequenceStatus::SUCCESS;
+    response.responseType = ResponseType::NONE;
 
     if (activeSequences.size() != 0)
     {
@@ -135,13 +136,53 @@ SequenceStatus GestureSeqRecorder::ensureSameState(ControlState state)
 
 SequenceStatus GestureSeqRecorder::progressActiveSequences(myo::Pose::Type gesture, ControlState state, sequenceResponse& response)
 {
-    //TODO
+    SequenceStatus status = SequenceStatus::SUCCESS;
+
+    clock_t now = clock();
+
+    if (now - progressBaseTime > progressMaxDeltaTime)
+    {
+        // Timeout
+        emptyActiveSequences();
+        return status;
+    }
+    progressBaseTime = now;
+
+    std::list<sequenceInfo*>::iterator it;
+    for (it = activeSequences.begin(); it != activeSequences.end(); it++)
+    {
+        unsigned int seqProg = (*it)->progress;
+        if (gesture == (*it)->seq.at(seqProg))
+        {
+            (*it)->progress++;
+            if ((*it)->progress == (*it)->seq.size())
+            {
+                // found a complete sequence!
+                // NOTE: This is guaranteed to be the ONLY sequence that will match the input
+                // gesture, due to how sequences are registered. (it is a pre-condition for this function).
+
+                response = (*it)->sequenceResponse;
+                break;
+            }
+        }
+        else
+        {
+            // TODO - verify that this is legal. MAY need to use a different structure than a list if it's not.
+            (*it)->progress = 0;
+            activeSequences.remove((*it));
+        }
+    }
+
+    return status;
 }
 
 SequenceStatus GestureSeqRecorder::findActivation(myo::Pose::Type gesture, ControlState state, sequenceResponse& response)
 {
     SequenceStatus status = SequenceStatus::SUCCESS;
     sequenceList *seqList = (*seqMapPerMode)[state.getMode()];
+
+    clock_t now = clock();
+    progressBaseTime = now;
 
     // Loop through all possible sequences in this mode, and activate any that
     // have a matching first gesture.
@@ -155,7 +196,7 @@ SequenceStatus GestureSeqRecorder::findActivation(myo::Pose::Type gesture, Contr
 
                 // 2 cases - If the sequence is only size one, then the response can be set, as the 
                 // sequence is complete. 
-                // NOTE: This is guarunteed to be the ONLY size one sequence that will match the input
+                // NOTE: This is guaranteed to be the ONLY size one sequence that will match the input
                 // gesture, due to how sequences are registered. (it is a pre-condition for this function).
                 if (it->seq.size() == 1)
                 {
