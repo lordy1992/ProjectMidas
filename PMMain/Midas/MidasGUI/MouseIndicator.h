9e4c7c4a9e6a772addfc4660fbadafe23e15da70
lordy1992 <jlord_2006@hotmail.com>
2015-01-22 15:15:50 -0800

    Removing keyboard event code from mouse indicator

--- PMMain/Midas/MidasGUI/MouseIndicator.h
+++ PMMain/Midas/MidasGUI/MouseIndicator.h
@@ -12,9 +12,6 @@ public:
     MouseIndicator(MidasThread *mainThread, int deadZoneRad, int widgetWidth = 100, int widgetHeight = 100, QWidget *parent = 0);
     QSize sizeHint() const;
 
-    // For testing handleUpdateCursorPos, until we plug it into the Myo code.
-    void keyPressEvent(QKeyEvent *e);
-
 protected:
     void paintEvent(QPaintEvent *event);
     void resizeEvent(QResizeEvent *event);
