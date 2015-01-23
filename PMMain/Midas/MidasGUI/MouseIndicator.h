f1c3ea961b65508e2dc71abb90346f8d9d279b37
lordy1992 <jlord_2006@hotmail.com>
2015-01-22 16:30:51 -0800

    Added doxygen comments to the GUI classes

--- PMMain/Midas/MidasGUI/MouseIndicator.h
+++ PMMain/Midas/MidasGUI/MouseIndicator.h
@@ -4,16 +4,41 @@
 #include "DraggableWidget.h"
 #include "MidasThread.h"
 
+/**
+ * The MouseIndicator class represents a circular GUI which tracks Midas mouse velocity. The GUI supports a
+ * 'dead zone' for the mouse, which looks like a smaller circle within the display. It shows a small cursor
+ * that moves about that represents the current mouse velocity.
+ */
 class MouseIndicator : public DraggableWidget
 {
     Q_OBJECT
 
 public:
+    /**
+     * The constructor for the MouseIndicator. 
+     *
+     * @param mainThread The main thread of the Midas application; used to retrieve mouse velocity information.
+     * @param deadZoneRad The radius of the dead zone, expressed as a percentage of the range of possible velocity values.
+     * @param widgetWidth The width of the widget.
+     * @param widgetHeight The height of the widget.
+     * @param parent The parent widget.
+     */
     MouseIndicator(MidasThread *mainThread, int deadZoneRad, int widgetWidth = 100, int widgetHeight = 100, QWidget *parent = 0);
     QSize sizeHint() const;
 
 protected:
+    /**
+     * The event that is called when the GUI needs to paint itself.
+     *
+     * @param event The information about the paint event.
+     */
     void paintEvent(QPaintEvent *event);
+
+    /**
+     * The event that is called when the widget is resized.
+     *
+     * @param event The information about the resize event.
+     */
     void resizeEvent(QResizeEvent *event);
 
 private:
@@ -23,6 +48,15 @@ private:
     MidasThread *mainThread;
 
 public slots:
+    /**
+     * A slot that updates the cursor position based on the provided 'velocity percentages'. These 
+     * velocity percentages are expressed as a percent of the maximum velocity, ranging
+     * from -100 to 100, where -100 represents the maximum velocity in the negative direction and
+     * +100 represents the maximum velocity in the positive direction.
+     *
+     * @param percentX A percent of the maximum x velocity, where the sign indicates direction, between -100 and 100.
+     * @param percentY A percent of the maximum y velocity, where the sign indicates direction, between -100 and 100.
+     */
     void handleUpdateCursorPos(int percentX, int percentY);
 };
 
--- PMMain/Midas/MidasGUI/SequenceDisplayer.h
+++ PMMain/Midas/MidasGUI/SequenceDisplayer.h
@@ -14,24 +14,75 @@
 Q_DECLARE_METATYPE(std::vector<sequenceImageSet>)
 Q_DECLARE_METATYPE(std::vector<sequenceProgressData>)
 
-// A grid with 5 columns, and a certain number of rows.
+/**
+ * The SequenceDisplayer class represents a GUI that can display Midas gesture sequences. These gesture sequences
+ * are made up of a series of images that should convey information to the user on what gesture they must perform
+ * next in the sequence.
+ */
 class SequenceDisplayer : public DraggableWidget
 {
     Q_OBJECT
 
 public:
+    /**
+     * Constructor for the SequenceDisplayer.
+     *
+     * @param parent The parent widget.
+     */
     SequenceDisplayer(QWidget *parent = 0);
 
+    /**
+     * Returns the recommended size of the widget.
+     *
+     * @return The recommended size of the widget.
+     */
     QSize sizeHint() const;
 
 public slots:
+    /**
+     * Register a sequence, with an ID, name, and set of images.
+     *
+     * @param id The id of the sequence.
+     * @param name A human-readable name for the sequence.
+     * @param images The images that make up the sequence. Each element of the vector contains two images.
+     */
     void registerSequenceImages(int, QString, std::vector<sequenceImageSet>);
+
+    /**
+     * Show the provided sequences on the display. The caller supplied a vector of pairs; each pair has
+     * a sequence ID and a number that represents the current position in the sequence.
+     *
+     * @param sequenceIdProgressPairs A vector containing the pairs of sequence IDs and sequence positions.
+     */
     void showSequences(std::vector<sequenceProgressData>);
  
 private:
+    /**
+     * Clear the widgets in the grid layout.
+     *
+     * @param deleteLabels If true, deletes the labels in the sequences to free memory. Otherwise, only hides the 
+     *        labels in the layout.
+     */
     void clearWidgets(bool deleteLabels = false);
+
+    /**
+     * Clears the widgets in a row in the grid layout.
+     *
+     * @param seq The row of the grid layout to clear.
+     * @param deleteLabels If true, deletes the labels in the row to free memory. Otherwise, only hides the labels.
+     */
     void clearRow(sequenceData seq, bool deleteLabels = false);
+
+    /**
+     * Sets the properties of the provided label to conform to the look of the members of the grid layout.
+     *
+     * @param label The label to set properties for.
+     */
     void formBoxLabel(QLabel *label);
+
+    /**
+     * Updates the GUI with the new active sequence information.
+     */
     void updateSequences();
 
     std::map<int, sequenceData> sequenceIdToDataMap;
