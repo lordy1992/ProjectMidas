6d0060e9ee11b82f83b5d74658af96e6037f4cb4
owen927 <owen-927@hotmail.com>
2015-02-07 15:57:32 -0800

    MidasGUITest compiling

--- PMMain/Midas/MidasGUIQTest/KeyboardSettingFileReader_Test.cpp
+++ PMMain/Midas/MidasGUIQTest/KeyboardSettingFileReader_Test.cpp
@@ -1,1 +1,11 @@
 #include "KeyboardSettingsReader.h"
+#include "RingData.h"
+#include <gtest\gtest.h>
+
+TEST(KeyboardFileReaderTest, constructor) {
+    std::vector<ringData>  test;
+    KeyboardSettingsReader reader;
+    reader.readKeyboardSetupFile(test);
+
+    ASSERT_EQ(test[0].getRingInVectorHandle()->at(0).main,'a');
+}
