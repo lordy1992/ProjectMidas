c34784f710a181c318d187061324ef9bce0e7ab6
owen927 <owen-927@hotmail.com>
2015-02-09 12:49:26 -0800

    fixed weird paths

--- PMMain/Midas/MidasGUIQTest/KeyboardSettingFileReader_Test.cpp
+++ PMMain/Midas/MidasGUIQTest/KeyboardSettingFileReader_Test.cpp
@@ -3,9 +3,10 @@
 #include <gtest\gtest.h>
 
 TEST(KeyboardFileReaderTest, constructor) {
-   // std::vector<ringData>  test;
-   // KeyboardSettingsReader reader;
-  // reader.readKeyboardSetupFile(test);
+    
+    std::vector<ringData>  test;
+    KeyboardSettingsReader reader;
+    reader.readKeyboardSetupFile(test);
 
-//    ASSERT_EQ(test[0].getRingInVectorHandle()->at(0).main,'a');
+    ASSERT_EQ(test.size(),2);
 }
