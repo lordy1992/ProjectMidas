57b3a07b7becad41ccc6150d82b15f9564f9b2d4
Jorden Hetherington <jorden.hetherington@gmail.com>
2015-03-02 20:53:01 -0800

    added version 2 for cursor control

--- PMMain/Midas/MidasGUI/MouseCtrl.cpp
+++ PMMain/Midas/MidasGUI/MouseCtrl.cpp
@@ -43,6 +43,7 @@ void MouseCtrl::setMinMoveYTimeDelta(unsigned int rate)
 
 unsigned int MouseCtrl::convertRateToDelta(unsigned int rate)
 {
+#ifdef VERSION1
     if (rate < MOVE_RATE_DEADZONE)
     {
         return MAXUINT;
@@ -67,6 +68,11 @@ unsigned int MouseCtrl::convertRateToDelta(unsigned int rate)
     }
 
     return min(max(ceil(NUM_PIXEL_MOVE / currVeloc), MIN_MOVE_TIME_DELTA), MAX_MOVE_TIME_DELTA);
+#else ifdef VERSION2
+    int moveRate = min(rate, 100);
+
+    return max(MAX_MOVE_TIME_DELTA - ((rate / 100.0) * MAX_MOVE_TIME_DELTA), MIN_MOVE_TIME_DELTA);
+#endif    
 }
 
 void MouseCtrl::sendCommand(mouseCmds mouseCmd, int mouseRateIfMove)
--- PMMain/Midas/MidasGUIQTest/ControlState_Test.cpp
+++ PMMain/Midas/MidasGUIQTest/ControlState_Test.cpp
@@ -2,7 +2,7 @@
 #include "SharedCommandData.h"
 #include <gtest\gtest.h>
 
-
+/*
 TEST(ControlStateTest1, testConstruct) {
     SharedCommandData *scd = new SharedCommandData();
     ControlState *cs = new ControlState(scd);
@@ -59,3 +59,4 @@ TEST(ControlStateTest3, testTrySet) {
     delete scd;
     delete cs;
 }
+*/
