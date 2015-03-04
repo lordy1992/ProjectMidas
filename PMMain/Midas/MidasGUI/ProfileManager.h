21b0b4e0425bc71995aae81909c432b2c0582f7a
lordy1992 <jlord_2006@hotmail.com>
2015-03-03 16:47:40 -0800

    Populating data structure from profiles xml file

--- PMMain/Midas/MidasGUI/ProfileManager.h
+++ PMMain/Midas/MidasGUI/ProfileManager.h
@@ -2,7 +2,29 @@
 #define PROFILE_MANAGER_H
 
 #include <string>
+#include <vector>
 #include <boost/property_tree/ptree.hpp>
+
+struct command {
+    std::string type;
+    std::vector<std::string> actions;
+};
+
+struct gesture {
+    std::string type;
+    std::string name;
+};
+
+struct sequence {
+    std::string state;
+    std::vector<gesture> gestures;
+    command cmd;
+};
+
+struct profile {
+    std::string profileName;
+    std::vector<sequence> sequences;
+};
 
 class ProfileManager {
 
@@ -13,7 +35,9 @@ public:
     void loadProfilesFromFile(std::string fileName);
 
 private:
-    void extractProfileInformation(boost::property_tree::ptree::value_type const & vt, std::string profileName);
+    profile extractProfileInformation(const boost::property_tree::ptree::value_type & parentProfile, std::string profileName);
+    sequence extractSequenceInformation(const boost::property_tree::ptree::value_type & parentSequence, std::string sequenceState);
+    std::vector<profile> profiles;
 };
 
 #endif /* PROFILE_MANAGER_H */
